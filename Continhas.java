c4a465dc4f9643f80583b203499ac7172cd1a1e1
Henry Callera <henrycalleraa@gmail.com>
2025-03-10 11:46:00 -0300

    fundamentos de Tipos de Variáveis em Java

---
+++ Continhas.java
@@ -0,0 +1,22 @@
+public class Continhas {
+    public static void main(String [] args){
+        int a,b,c;
+        float d;
+        double e;
+
+        a = 27;
+        b =4;
+        c = a +b;
+        System.out.println("Valor de C = " + c);
+        c = a-b;
+        System.out.println("Valor de C = " +c);
+        c = a*b;
+        System.out.println("Valor de C = " +c);
+        c = a/b;
+        System.out.println("Valor de C = " +c);
+        c = a % b;
+        System.out.println("Resto da divisao de " + a +" por " + b +" = " + c);
+
+        d = 1.0f;
+    }
+}
